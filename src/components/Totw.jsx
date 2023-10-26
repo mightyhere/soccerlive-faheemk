75e93541b12e04d2a0e96530589807b896373fe6
faheemgarrix <faheemgarrix@gmail.com>
2023-10-26 15:16:34 +0530

    changes

--- src/components/Totw.jsx
+++ src/components/Totw.jsx
@@ -17,7 +17,7 @@ function Totw() {
         </button>
       </div>
      <div className='  sm:p-5 sm:mt-4 '>
-     <div className='absolute bg-[#BDE1CE] rounded-md w-[22rem] h-[38rem] ' style={{ backgroundImage: `url(${footballPitch})` }}>
+     <div className=' bg-[#BDE1CE] rounded-md w-[22rem] h-[38rem] ' style={{ backgroundImage: `url(${footballPitch})` }}>
        <div className=''>
        <div >
             <img src={Player} alt="" className='mt-4 ml-36' />
